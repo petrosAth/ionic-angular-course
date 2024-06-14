5f5261b65cf5b586560d0cf155d4a34f26369479
Petros Athanasoulis <develop@athanasoulis.me>
2024-06-14 16:03:26 +0300

    Extracting and Displaying Route Param Data

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -9,7 +9,7 @@ import { RecipesService } from './recipes.service';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[];
 
   constructor(private recipesService: RecipesService) {}
 
